d722ec76eb03271b49b7a55c6776206537b70b49
zlchangg@gmail.com <zlchangg@gmail.com>
2019-10-23 11:03:57 +0800

    z

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -19,7 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import sys
+import sys,time
 
 import Adafruit_DHT
 
@@ -36,9 +36,10 @@ else:
     print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
     sys.exit(1)
 
+
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
 
 # Un-comment the line below to convert the temperature to Fahrenheit.
 # temperature = temperature * 9/5.0 + 32
@@ -47,8 +48,11 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # the results will be null (because Linux can't
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try again!')
-    sys.exit(1)
+while True:
+	h0,t0=Adafruit_DHT.read_retry(sensor,pin)
+	if h0 is not None and t0 is not None:
+    		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(t0,h0))
+		time.sleep(0.1)
+	else:
+    		print('Failed to get reading. Try again!')
+    		sys.exit(1)
